0ceb1bf8418784df057773b499f2fc622ced6606
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-26 17:30:27 -0500

    alert displays input value

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-$(document).ready(function (){
-    
-})
+$(document).ready(function() {
+    $('#btnSubmit').click(function(e){
+    //   alert('dgfhghfgh');
+   // });
+
+    $('form').submit(function(e){
+        e.preventDefault();
+        alert($("input[name='name']").val());
+
+       $("div").append($(`<h2>${$("input[name='name']").val()}</h2>`));
+      $("h2").mouseover(function (e){
+           $(e.target).css({
+             "background-color": random_bg_color(),
+              "border-radius": (Math.random() * 50) + "px"
+        })
+     });
+
+
+
+    });
+});
+});
