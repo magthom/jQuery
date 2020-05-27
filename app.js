205b8b12a69ab908e1039eee9acaad81c70f09cd
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-27 09:10:30 -0500

    random bg colors working

--- app.js
+++ app.js
@@ -9,31 +9,32 @@ $(document).ready(function() {
 
     $("div").append($(`<h2>${$("input[name='name']").val()}</h2>`));
     $("h2").mouseover(function () {
-           $(this).css("background-color", "blue")
-         //"background-color": 'blue'
-         //"border-radius": (Math.random() * 50) + "px"
-           });
-        
-    
+        $(this).css({
+            "background-color" : random_bg_color(), 
+            "border-radius" : (Math.random() * 50) + "px" 
+        });
+    });
+});
 
-     
-
-
-
-    
-
-
-  //$("input[name='name']").on("keyup", function (){
-       //if ($("input[name='name']").val() === "") {
-         //  $('#btnSubmit').attr("disabled", true);    
-       // } else {
-         //  $('#btnSubmit').attr("disabled", false);
-       // }
-    //})
-    $("body").append($("<form></form>"));
+  $("input[name='name']").on("keyup", function (){
+        if ($("input[name='name']").val() === "") {
+        $('#btnSubmit').attr("disabled", true);    
+    } else {
+        $('#btnSubmit').attr("disabled", false);
+    }
+    });
+   
     $("body").append($("<div></div>"));
 
 
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ return bgColor
+}
 });
-});
+
 
