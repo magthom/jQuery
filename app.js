015a654cef312a944f7c07f16f6d036e86047d27
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-27 09:20:21 -0500

    remove text when dbl click

--- app.js
+++ app.js
@@ -1,40 +1,45 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //$('#btnSubmit').click(function(e){
     //   alert('dgfhghfgh');
-   // });
+    // });
 
-    $("form").submit(function(e){
+    $("form").submit(function (e) {
         e.preventDefault();
         //($("input[name='name']").val());
 
-    $("div").append($(`<h2>${$("input[name='name']").val()}</h2>`));
-    $("h2").mouseover(function () {
-        $(this).css({
-            "background-color" : random_bg_color(), 
-            "border-radius" : (Math.random() * 50) + "px" 
+        //    $("div").append($(`<h2>${$("input[name='name']").val()}</h2>`));
+        //    $("h2").mouseover(function () {
+        //        $(this).css({
+        //            "background-color" : random_bg_color(), 
+        //            "border-radius" : (Math.random() * 50) + "px" 
+        //        });
+        //    });
+        $("ul").append($(`<li>${$("input[name='name']").val()}</li>`));
+        $("li").click(function (e){
+            $(e.target).css("background-color", random_bg_color());
+        });
+        $("li").dblclick(function (e){
+            $(e.target).remove();
         });
     });
-});
+    
 
-  $("input[name='name']").on("keyup", function (){
+    $("input[name='name']").on("keyup", function () {
         if ($("input[name='name']").val() === "") {
-        $('#btnSubmit').attr("disabled", true);    
-    } else {
-        $('#btnSubmit').attr("disabled", false);
-    }
+            $('#btnSubmit').attr("disabled", true);
+        } else {
+            $('#btnSubmit').attr("disabled", false);
+        }
     });
-   
+
     $("body").append($("<div></div>"));
-
-
-
-function random_bg_color() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
- return bgColor
-}
+    $("body").append($("<ul></ul>"));
 });
 
-
+    function random_bg_color() {
+        var x = Math.floor(Math.random() * 256);
+        var y = Math.floor(Math.random() * 256);
+        var z = Math.floor(Math.random() * 256);
+        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+        return bgColor
+    }
